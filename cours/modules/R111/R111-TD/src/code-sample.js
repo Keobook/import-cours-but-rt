a4ac40339544c164d92bf6ca74055ec1c909ea80
Alexis Opolka <alexis.opolka@gmail.com>
2022-10-24 17:05:34 +0200

    Add R111-TD

---
+++ cours/modules/R111/R111-TD/src/code-sample.js
@@ -0,0 +1,6 @@
+function mesDefauts(){
+  var defautPrincipal = "Indécis";
+  var defautSecondaire = "tétu";
+
+  console.log("Mes defauts sont:", defautPrincipal, defautSecondaire);
+}
---
+++ cours/modules/R111/R111-TD/src/code-sample.php
@@ -0,0 +1,8 @@
+<?php
+
+function getmyparcours(){
+  echo "Je viens d'une filière générale et ait fait mon baccalauréat au lycée Marc bloch de Sérignan, 34410.";
+}
+
+getmyparcours();
+?>
