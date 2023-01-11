2d8c7d907efb7d98d5309daf2d9c15a0912c527b
alexis-opolka <alexis.opolka@proton.me>
2023-01-11 08:10:20 +0100

    Created Jupyter file

--- cours/sae/sae-15/sae-15-TP/sae-15-TP1/prog.py
+++ cours/sae/sae-15/sae-15-TP/sae-15-TP1/prog.py
@@ -29,7 +29,7 @@ def requestThenWrite(request: str, _dir: str = None):
 
 ### Question 1
 print("Question 1:")
-requestThenWrite("https://data.montpellier3m.fr/sites/default/files/ressources/FR_MTP_COME.xml")
+requestThenPrint("https://data.montpellier3m.fr/sites/default/files/ressources/FR_MTP_COME.xml")
 
 
 
@@ -38,4 +38,4 @@ print("\n\nQuestion 2:")
 parkings = ['FR_MTP_ANTI.xml','FR_MTP_COME.xml','FR_MTP_CORU.xml','FR_MTP_EURO.xml','FR_MTP_FOCH.xml','FR_MTP_GAMB.xml','FR_MTP_GARE.xml','FR_MTP_TRIA.xml','FR_MTP_ARCT.xml','FR_MTP_PITO.xml','FR_MTP_CIRC.xml','FR_MTP_SABI.xml','FR_MTP_GARC.xml','FR_CAS_SABL.xml','FR_MTP_MOSS.xml','FR_STJ_SJLC.xml','FR_MTP_MEDC.xml','FR_MTP_OCCI.xml','FR_CAS_VICA.xml','FR_MTP_GA109.xml','FR_MTP_GA250.xml','FR_CAS_CDGA.xml','FR_MTP_ARCE.xml','FR_MTP_POLY.xml']
 
 for parking in parkings :
-  requestThenPrint(f"https://data.montpellier3m.fr/sites/default/files/ressources/{parking}")
+  requestThenWrite(f"https://data.montpellier3m.fr/sites/default/files/ressources/{parking}")
