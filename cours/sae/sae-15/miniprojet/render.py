44df200349eaabfbd79a9e4748de9467861014d0
centaurus <alexis.opolka@protonmail.com>
2023-01-19 07:13:41 +0000

    Add render script (nearly finished)

--- cours/sae/sae-15/miniprojet/render.py
+++ cours/sae/sae-15/miniprojet/render.py
@@ -1,11 +1,13 @@
+### pip install influxdb-client
+
 import influxdb_client
 from influxdb_client.client.write_api import SYNCHRONOUS
 
-bucket = "parking"
+bucket = "parkings"
 org = "iut-students"
 token = "C-y-3-UfAZCL6oqxq5vCorwdbI2_XKv7BkMjTRURe_dXGcALUOCrzVqrt0aHpONaPJ6UlGYUBsFJInMIJ5n1NA=="
 # Store the URL of your InfluxDB instance
-url = "http://188.166.151.235/"
+url = "http://188.166.151.235:8086/"
 
 client = influxdb_client.InfluxDBClient(
     url=url,
@@ -15,9 +17,40 @@ client = influxdb_client.InfluxDBClient(
 
 write_api = client.write_api(write_options=SYNCHRONOUS)
 
-with open("./in/stats/csv/FR_MTP_GARE.CSV") as fin:
-  fin = fin.readlines()
-  
+parkings = [
+  'FR_MTP_ANTI', 'FR_MTP_COME', 'FR_MTP_CORU', 'FR_MTP_EURO', 'FR_MTP_FOCH',
+  'FR_MTP_GAMB', 'FR_MTP_GARE', 'FR_MTP_TRIA', 'FR_MTP_ARCT', 'FR_MTP_PITO',
+  'FR_MTP_CIRC', 'FR_MTP_SABI', 'FR_MTP_GARC', 'FR_MTP_SABL', 'FR_MTP_MOSS',
+  'FR_STJ_SJLC', 'FR_MTP_MEDC', 'FR_MTP_OCCI', 'FR_CAS_VICA', 'FR_MTP_GA109',
+  'FR_MTP_GA250', 'FR_CAS_CDGA', 'FR_MTP_ARCE', 'FR_MTP_POLY'
+]
 
-p = influxdb_client.Point("").tag("location", "Prague").field("temperature", 25.3)
-write_api.write(bucket=bucket, org=org, record=p)
+for parking in parkings:
+  print(f"Working on {parking}...", end=" ")
+  with open(f"./in/stats/csv/{parking}.CSV", "rt", encoding="utf-8") as fin:
+    print("Parsing, please wait...", end="")
+    fin = fin.readlines()[1:]
+    print("The current size of the file is:", len(fin), "lines.")
+    for i in range(0, len(fin)):
+      data = fin[i].strip().split(",")
+
+      if len(data) > 1:
+        ### CSV Header: name,date,export_date,opened,free_places,places
+        db = {
+          "name": data[0],
+          "export_date": data[1],
+          "real_date": data[2],
+          "is_opened": bool(data[3]),
+          "availability": int(data[4]),
+          "capacity": int(data[5])
+        }
+
+        p = influxdb_client.Point(db["name"])
+
+        p.field("export_date", db["export_date"])
+        p.field("real_date", db["real_date"])
+        p.field("is_opened", db["is_opened"])
+        p.field("availability", db["availability"])
+        p.field("capacity", db["capacity"])
+        p.time(db["real_date"])
+        write_api.write(bucket=bucket, org=org, record=p)
