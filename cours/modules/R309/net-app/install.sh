4adccc45dabfe72d4b0df795d807a0f3e1b59767
Alexis Opolka <53085471+alexis-opolka@users.noreply.github.com>
2023-12-12 11:41:22 +0100

    Went from cURL to Wget and fix URLs

--- cours/modules/R309/net-app/install.sh
+++ cours/modules/R309/net-app/install.sh
@@ -6,15 +6,21 @@ curl -O --output-dir ./net-app/ https://raw.githubusercontent.com/alexis-opolka/
 curl -O --output-dir ./net-app/ https://raw.githubusercontent.com/alexis-opolka/import-cours-but-rt/master/cours/modules/R309/net-app/network_link.py
 curl -O --output-dir ./net-app/ https://raw.githubusercontent.com/alexis-opolka/import-cours-but-rt/master/cours/modules/R309/net-app/utils.py
 curl -O --output-dir ./net-app/ https://raw.githubusercontent.com/alexis-opolka/import-cours-but-rt/master/cours/modules/R309/net-app/coords.py
-curl -O --output-dir ./net-app/src/icons/ https://raw.githubusercontent.com/alexis-opolka/import-cours-but-rt/master/cours/modules/R309/net-app/src/icons/mobile.png
-curl -O --output-dir ./net-app/src/icons/ https://raw.githubusercontent.com/alexis-opolka/import-cours-but-rt/master/cours/modules/R309/net-app/src/icons/pc.png
-curl -O --output-dir ./net-app/src/icons/ https://raw.githubusercontent.com/alexis-opolka/import-cours-but-rt/master/cours/modules/R309/net-app/src/icons/router.png
-curl -O --output-dir ./net-app/src/icons/ https://raw.githubusercontent.com/alexis-opolka/import-cours-but-rt/master/cours/modules/R309/net-app/src/icons/switch.png
-curl -O --output-dir ./net-app/src/icons/alternatives/ https://raw.githubusercontent.com/alexis-opolka/import-cours-but-rt/master/cours/modules/R309/net-app/src/icons/alternatives/mobile.drawio.png
-curl -O --output-dir ./net-app/src/icons/alternatives/ https://raw.githubusercontent.com/alexis-opolka/import-cours-but-rt/master/cours/modules/R309/net-app/src/icons/alternatives/pc.drawio.png
-curl -O --output-dir ./net-app/src/icons/alternatives/ https://raw.githubusercontent.com/alexis-opolka/import-cours-but-rt/master/cours/modules/R309/net-app/src/icons/alternatives/router.drawio.png
-curl -O --output-dir ./net-app/src/icons/alternatives/ https://raw.githubusercontent.com/alexis-opolka/import-cours-but-rt/master/cours/modules/R309/net-app/src/icons/alternatives/router.png
-curl -O --output-dir ./net-app/src/icons/alternatives/ https://raw.githubusercontent.com/alexis-opolka/import-cours-but-rt/master/cours/modules/R309/net-app/src/icons/alternatives/switch.drawio.png
-curl -O --output-dir ./net-app/src/icons/alternatives/ https://raw.githubusercontent.com/alexis-opolka/import-cours-but-rt/master/cours/modules/R309/net-app/src/icons/alternatives/switch.png
 
-cd ./net-app/
+cd ./net-app/src/icons
+
+wget https://github.com/alexis-opolka/import-cours-but-rt/raw/master/cours/modules/R309/net-app/src/icons/mobile.png?download= -O mobile.png
+wget https://github.com/alexis-opolka/import-cours-but-rt/raw/master/cours/modules/R309/net-app/src/icons/pc.png?download= -O pc.png
+wget https://github.com/alexis-opolka/import-cours-but-rt/raw/master/cours/modules/R309/net-app/src/icons/router.png?download= -O router.png
+wget https://github.com/alexis-opolka/import-cours-but-rt/raw/master/cours/modules/R309/net-app/src/icons/switch.png?download= -O switch.png
+
+cd ./alternatives/
+
+wget https://github.com/alexis-opolka/import-cours-but-rt/raw/master/cours/modules/R309/net-app/src/icons/alternatives/mobile.drawio.png?download= -O mobile.drawio.png
+wget https://github.com/alexis-opolka/import-cours-but-rt/raw/master/cours/modules/R309/net-app/src/icons/alternatives/pc.drawio.png?download= -O pc.drawio.png
+wget https://github.com/alexis-opolka/import-cours-but-rt/raw/master/cours/modules/R309/net-app/src/icons/alternatives/router.drawio.png?download= -O router.drawio.png
+wget https://github.com/alexis-opolka/import-cours-but-rt/raw/master/cours/modules/R309/net-app/src/icons/alternatives/router?download= -O router.png
+wget https://github.com/alexis-opolka/import-cours-but-rt/raw/master/cours/modules/R309/net-app/src/icons/alternatives/switch.drawio.png?download= -O switch.drawio.png
+wget https://github.com/alexis-opolka/import-cours-but-rt/raw/master/cours/modules/R309/net-app/src/icons/alternatives/switch.png?download= -O switch.png
+
+cd ../../
