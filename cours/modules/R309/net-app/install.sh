50328271eddcc7c041ac624453e3e49d273e9367
Alexis Opolka <53085471+alexis-opolka@users.noreply.github.com>
2023-12-12 11:12:15 +0100

    Update Install script

--- cours/modules/R309/net-app/install.sh
+++ cours/modules/R309/net-app/install.sh
@@ -1,8 +1,20 @@
 #! /usr/bin/sh
 
 mkdir -p ./net-app/src/icons/alternatives/
-curl -O --output-dir ./net-app/ https://raw.githubusercontent.com/alexis-opolka/import-cours-but-rt/master/cours/modules/R309/net-app/[net-app,network_equipment,network_link,utils,coords].py
-curl -O --output-dir ./net-app/src/icons/ https://raw.githubusercontent.com/alexis-opolka/import-cours-but-rt/master/cours/modules/R309/net-app/src/icons/[mobile,pc,router,switch].png
-curl -O --output-dir ./net-app/src/icons/alternatives/ https://raw.githubusercontent.com/alexis-opolka/import-cours-but-rt/master/cours/modules/R309/net-app/src/icons/alternatives/[mobile.drawio,pc.drawio,router.drawio,router,switch.drawio,switch].png
+curl -O --output-dir ./net-app/ https://raw.githubusercontent.com/alexis-opolka/import-cours-but-rt/master/cours/modules/R309/net-app/net-app.py
+curl -O --output-dir ./net-app/ https://raw.githubusercontent.com/alexis-opolka/import-cours-but-rt/master/cours/modules/R309/net-app/network_equipment.py
+curl -O --output-dir ./net-app/ https://raw.githubusercontent.com/alexis-opolka/import-cours-but-rt/master/cours/modules/R309/net-app/network_link.py
+curl -O --output-dir ./net-app/ https://raw.githubusercontent.com/alexis-opolka/import-cours-but-rt/master/cours/modules/R309/net-app/utils.py
+curl -O --output-dir ./net-app/ https://raw.githubusercontent.com/alexis-opolka/import-cours-but-rt/master/cours/modules/R309/net-app/coords.py
+curl -O --output-dir ./net-app/src/icons/ https://raw.githubusercontent.com/alexis-opolka/import-cours-but-rt/master/cours/modules/R309/net-app/src/icons/mobile.png
+curl -O --output-dir ./net-app/src/icons/ https://raw.githubusercontent.com/alexis-opolka/import-cours-but-rt/master/cours/modules/R309/net-app/src/icons/pc.png
+curl -O --output-dir ./net-app/src/icons/ https://raw.githubusercontent.com/alexis-opolka/import-cours-but-rt/master/cours/modules/R309/net-app/src/icons/router.png
+curl -O --output-dir ./net-app/src/icons/ https://raw.githubusercontent.com/alexis-opolka/import-cours-but-rt/master/cours/modules/R309/net-app/src/icons/switch.png
+curl -O --output-dir ./net-app/src/icons/alternatives/ https://raw.githubusercontent.com/alexis-opolka/import-cours-but-rt/master/cours/modules/R309/net-app/src/icons/alternatives/mobile.drawio.png
+curl -O --output-dir ./net-app/src/icons/alternatives/ https://raw.githubusercontent.com/alexis-opolka/import-cours-but-rt/master/cours/modules/R309/net-app/src/icons/alternatives/pc.drawio.png
+curl -O --output-dir ./net-app/src/icons/alternatives/ https://raw.githubusercontent.com/alexis-opolka/import-cours-but-rt/master/cours/modules/R309/net-app/src/icons/alternatives/router.drawio.png
+curl -O --output-dir ./net-app/src/icons/alternatives/ https://raw.githubusercontent.com/alexis-opolka/import-cours-but-rt/master/cours/modules/R309/net-app/src/icons/alternatives/router.png
+curl -O --output-dir ./net-app/src/icons/alternatives/ https://raw.githubusercontent.com/alexis-opolka/import-cours-but-rt/master/cours/modules/R309/net-app/src/icons/alternatives/switch.drawio.png
+curl -O --output-dir ./net-app/src/icons/alternatives/ https://raw.githubusercontent.com/alexis-opolka/import-cours-but-rt/master/cours/modules/R309/net-app/src/icons/alternatives/switch.png
 
 cd ./net-app/
